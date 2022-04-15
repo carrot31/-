28ce2705fb905199875d4f277cc1ef53c6badc66
DESKTOP-OUB7GMG\carrot31 <jungwon0508@naver.com>
2022-04-15 09:21:52 +0900

    test branch

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>유정님 깃허브 연습하쟝</p>
+      <p>머지 성공했음 좋겠당히히</p>
+      <p>ㅊㄴ천히 하세영히히힣</p>
+      <p>너무 광기가태여? 24분남았어여</p>
+      <p>다음 내 오예 일단 팀장아님ㅜ</p>
     </div>
   );
 }
